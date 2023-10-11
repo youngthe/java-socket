01e879777daa4a50fde1bc591d2995e5a2a8339d
whdudwns7100 <whdudwns7100@naver.com>
2023-10-11 11:32:52 +0900

    final socket server

--- src/main/java/org/example/SocketServer.java
+++ src/main/java/org/example/SocketServer.java
@@ -3,12 +3,9 @@ package org.example;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class SocketServer {
-
-
 
 
     public static void main(String[] args) throws IOException {
@@ -17,6 +14,7 @@ public class SocketServer {
 
         socketServer.start();
 
+        return;
     }
 
     public void start(){
@@ -24,11 +22,10 @@ public class SocketServer {
         BufferedReader in = null;
         PrintWriter out = null;
 
-        boolean state;
-
         ServerSocket serverSocket = null;
         Socket socket = null;
         Scanner scanner = new Scanner(System.in);
+        boolean state;
 
         try {
             serverSocket = new ServerSocket(8000);
@@ -41,58 +38,39 @@ public class SocketServer {
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 out = new PrintWriter(socket.getOutputStream());
 
-
-
                 SocketServer socketServer = new SocketServer();
-                state = socketServer.pushData(in, out);
+                socketServer.pushData(in, out);
 
-                if(!state){
-                    scanner.close();		// Scanner 닫기
-                    socket.close();			// Socket 닫기
-                    serverSocket.close();		// ServerSocket 닫기
-
-                    serverSocket = new ServerSocket(8000);
-                    System.out.println("[Server실행] Client연결대기중...");
-                    socket = serverSocket.accept();			// 연결대기
-                    System.out.println("Client 연결됨.");
-                }
             }
-
 
         } catch (IOException e) {
             e.printStackTrace();
+            return;
         }
     }
 
     public boolean pushData(BufferedReader in, PrintWriter out) throws IOException {
 
-        String inputMessage = in.readLine();	// 수신데이터 한줄씩 읽기
+        try{
+            String inputMessage = in.readLine();	// 수신데이터 한줄씩 읽기
 
-        if(Objects.isNull(inputMessage)){
-
-            SocketServer socketServer = new SocketServer();
-            socketServer.start();
+            if(inputMessage.equals("getTemp")){
+                String value = String.format("%.1f", 20 * (Math.random()*0.2 + 1));
+                String value2 = String.format("%.1f",20 * (Math.random()*0.2f + 1));
+                String outputMessage = "<temp1_"+value+">,<temp2_"+value2+">";
+                out.println(outputMessage);
+                out.flush();
+                return true;
+            }
+            else{
+                String value = "me too";
+                out.println(value);
+                out.flush();
+                return true;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
             return false;
-        }
-
-        if(inputMessage.equals("test")){
-            String value = String.format("%.1f", 20 * (Math.random()*0.2 + 1));
-            String value2 = String.format("%.1f",20 * (Math.random()*0.2f + 1));
-            String outputMessage = "<temp1_"+value+">,<temp2_"+value2+">";
-            out.println(outputMessage);
-            out.flush();
-            return true;
-        }
-        else if(inputMessage.equals("hello")){
-            String value = "me too";
-            out.println(value);
-            out.flush();
-            return true;
-        }else{
-            String value = "error";
-            out.println(value);
-            out.flush();
-            return true;
         }
 
     }
